4bea3ee4f6b5820e7632369462643a2c7239bb53
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-01-03 20:51:10 +0900

    feat: 東北 6 県のラジオ局を追加

--- pkg/now_playing/radio_stations/tohoku.py
+++ pkg/now_playing/radio_stations/tohoku.py
@@ -1,6 +1,21 @@
 from now_playing.radio_stations import MtmDefaultRadioStation
 
 
+class FmAomoriRadioStation(MtmDefaultRadioStation):
+    """エフエム青森
+    """
+    tag_key: str = 'afb'
+
+    def get_radio_station_name(self) -> str:
+        return 'エフエム青森'
+
+    def get_station_url(self) -> str:
+        return 'https://afb.co.jp/'
+
+    def get_station_id(self) -> str:
+        return 'wu'
+
+
 class FmAkitaRadioStation(MtmDefaultRadioStation):
     """エフエム秋田
     """
@@ -14,6 +29,36 @@ class FmAkitaRadioStation(MtmDefaultRadioStation):
 
     def get_station_id(self) -> str:
         return 'pu'
+
+
+class FmIwateRadioStation(MtmDefaultRadioStation):
+    """エフエム岩手
+    """
+    tag_key: str = 'fmii'
+
+    def get_radio_station_name(self) -> str:
+        return 'エフエム岩手'
+
+    def get_station_url(self) -> str:
+        return 'https://www.fmii.co.jp/'
+
+    def get_station_id(self) -> str:
+        return 'qu'
+
+
+class FmYamagataRadioStation(MtmDefaultRadioStation):
+    """エフエム山形
+    """
+    tag_key: str = 'rfm'
+
+    def get_radio_station_name(self) -> str:
+        return 'エフエム山形'
+
+    def get_station_url(self) -> str:
+        return 'https://rfm.co.jp/'
+
+    def get_station_id(self) -> str:
+        return 'ev'
 
 
 class DateFmRadioStation(MtmDefaultRadioStation):
@@ -30,3 +75,18 @@ class DateFmRadioStation(MtmDefaultRadioStation):
 
     def get_station_id(self) -> str:
         return 'ju'
+
+
+class FmFukushimaRadioStation(MtmDefaultRadioStation):
+    """ふくしま FM
+    """
+    tag_key: str = 'fmf'
+
+    def get_radio_station_name(self) -> str:
+        return 'ふくしま FM'
+
+    def get_station_url(self) -> str:
+        return 'https://www.fmf.co.jp/'
+
+    def get_station_id(self) -> str:
+        return 'tv'
